9f416390cfbd30e72648807fc3d5d4008debca29
pengchenyu <1106998960@qq.com>
2021-01-03 20:33:33 +0800

    转换算子map——mapPartitionsWithIndex 操作指定分区内的数据.

---
+++ spark-core/src/main/java/com/pcy/bigdata/spark/core/rdd/operator/transform/Spark03_RDD_Operator_Transform.scala
@@ -0,0 +1,32 @@
+package com.pcy.bigdata.spark.core.rdd.operator.transform
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+ * mapPartitionsWithIndex 操作指定分区内的数据
+ */
+object Spark03_RDD_Operator_Transform {
+
+  def main(args: Array[String]): Unit = {
+
+    val sparkConf = new SparkConf().setMaster("local[*]").setAppName("Operator")
+    val sc = new SparkContext(sparkConf)
+
+    val rdd = sc.makeRDD(List(1, 2, 3, 4), 2)
+    // 【1，2】，【3，4】
+    val mpiRDD = rdd.mapPartitionsWithIndex(
+      (index, iter) => {
+        if (index == 1) {
+          iter
+        } else {
+          Nil.iterator
+        }
+      }
+    )
+    mpiRDD.collect().foreach(println)
+
+
+    sc.stop()
+
+  }
+}
---
+++ spark-core/src/main/java/com/pcy/bigdata/spark/core/rdd/operator/transform/Spark03_RDD_Operator_Transform1.scala
@@ -0,0 +1,32 @@
+package com.pcy.bigdata.spark.core.rdd.operator.transform
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+object Spark03_RDD_Operator_Transform1 {
+
+  def main(args: Array[String]): Unit = {
+
+    val sparkConf = new SparkConf().setMaster("local[*]").setAppName("Operator")
+    val sc = new SparkContext(sparkConf)
+
+    val rdd = sc.makeRDD(List(1, 2, 3, 4))
+
+    val mpiRDD = rdd.mapPartitionsWithIndex(
+      (index, iter) => {
+        // 1,   2,    3,   4
+        //(0,1)(2,2),(4,3),(6,4)
+        iter.map(
+          num => {
+            (index, num)
+          }
+        )
+      }
+    )
+
+    mpiRDD.collect().foreach(println)
+
+
+    sc.stop()
+
+  }
+}
