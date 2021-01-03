49a2eb919278099e44e8a384fdb0de00348d2fec
pengchenyu <1106998960@qq.com>
2021-01-03 20:46:18 +0800

    转换算子flatMap——flatMap 基本使用.

---
+++ spark-core/src/main/java/com/pcy/bigdata/spark/core/rdd/operator/transform/Spark04_RDD_Operator_Transform.scala
@@ -0,0 +1,32 @@
+package com.pcy.bigdata.spark.core.rdd.operator.transform
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+ * flatMap 基本使用
+ */
+object Spark04_RDD_Operator_Transform {
+
+  def main(args: Array[String]): Unit = {
+
+    val sparkConf = new SparkConf().setMaster("local[*]").setAppName("Operator")
+    val sc = new SparkContext(sparkConf)
+
+    val rdd: RDD[List[Int]] = sc.makeRDD(List(
+      List(1, 2), List(3, 4)
+    ))
+
+    val flatRDD = rdd.flatMap(
+      // 把rdd内的每个元素放到一个新的集合里
+      list => {
+        list
+      }
+    )
+    flatRDD.collect().foreach(println)
+
+
+    sc.stop()
+
+  }
+}
---
+++ spark-core/src/main/java/com/pcy/bigdata/spark/core/rdd/operator/transform/Spark04_RDD_Operator_Transform1.scala
@@ -0,0 +1,30 @@
+package com.pcy.bigdata.spark.core.rdd.operator.transform
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+ * flatMap 基本使用
+ */
+object Spark04_RDD_Operator_Transform1 {
+
+  def main(args: Array[String]): Unit = {
+
+    val sparkConf = new SparkConf().setMaster("local[*]").setAppName("Operator")
+    val sc = new SparkContext(sparkConf)
+
+    val rdd: RDD[String] = sc.makeRDD(List("Hello Scala", "Hello Spark"))
+
+    //      rdd.flatMap(
+    //          s => {
+    //              s.split(" ")
+    //          }
+    //      )
+    val flatRDD: RDD[String] = rdd.flatMap(_.split(" "))
+    flatRDD.collect().foreach(println)
+
+
+    sc.stop()
+
+  }
+}
