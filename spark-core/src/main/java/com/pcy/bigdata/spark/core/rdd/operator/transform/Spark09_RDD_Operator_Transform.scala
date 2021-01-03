2ccb7e80aca35be29718a66172bfe32219b72ed3
pengchenyu <1106998960@qq.com>
2021-01-03 22:32:35 +0800

    转换算子distinct——基本用法（注意实现方法）.

---
+++ spark-core/src/main/java/com/pcy/bigdata/spark/core/rdd/operator/transform/Spark09_RDD_Operator_Transform.scala
@@ -0,0 +1,36 @@
+package com.pcy.bigdata.spark.core.rdd.operator.transform
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+ * distinct算子
+ * 将数据集中重复的数据去重
+ *
+ */
+object Spark09_RDD_Operator_Transform {
+
+  def main(args: Array[String]): Unit = {
+
+    val sparkConf = new SparkConf().setMaster("local[*]").setAppName("Operator")
+    val sc = new SparkContext(sparkConf)
+
+    val rdd = sc.makeRDD(List(1, 2, 3, 4, 1, 2, 3, 4))
+
+    // Scala的做法：HashSet
+    // spark的做法：
+    // map(x => (x, null)).reduceByKey((x, _) => x, numPartitions).map(_._1)
+
+    // (1, null),(2, null),(3, null),(4, null),(1, null),(2, null),(3, null),(4, null)
+    // (1, null)(1, null)(1, null)
+    // (null, null) => null
+    // (1, null) => 1
+    val rdd1: RDD[Int] = rdd.distinct()
+
+    rdd1.collect().foreach(println)
+
+
+    sc.stop()
+
+  }
+}
