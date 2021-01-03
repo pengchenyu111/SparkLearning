fd4594636687be598bd74bef058105bac7721f4d
pengchenyu <1106998960@qq.com>
2021-01-03 20:50:42 +0800

    转换算子flatMap——flatMap 和模式匹配的使用

---
+++ spark-core/src/main/java/com/pcy/bigdata/spark/core/rdd/operator/transform/Spark04_RDD_Operator_Transform2.scala
@@ -0,0 +1,29 @@
+package com.pcy.bigdata.spark.core.rdd.operator.transform
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+ * flatMap 和模式匹配的使用
+ */
+object Spark04_RDD_Operator_Transform2 {
+
+  def main(args: Array[String]): Unit = {
+
+    val sparkConf = new SparkConf().setMaster("local[*]").setAppName("Operator")
+    val sc = new SparkContext(sparkConf)
+
+    val rdd = sc.makeRDD(List(List(1, 2), 3, List(4, 5)))
+
+    val flatRDD = rdd.flatMap {
+      case list: List[_] => list
+      case dat => List(dat)
+    }
+
+    flatRDD.collect().foreach(println)
+
+
+    sc.stop()
+
+  }
+}
