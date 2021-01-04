ce289d6cde0cf06f91c57774153df8e96a11893f
pengchenyu <1106998960@qq.com>
2021-01-04 17:51:46 +0800

    转换算子sortBy——排序基本用法.

---
+++ spark-core/src/main/java/com/pcy/bigdata/spark/core/rdd/operator/transform/Spark12_RDD_Operator_Transform.scala
@@ -0,0 +1,28 @@
+package com.pcy.bigdata.spark.core.rdd.operator.transform
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+ * sortBy算子
+ * 在排序之前，可以将数据通过 f 函数进行处理，之后按照 f 函数处理的结果进行排序，默认为升序排列。
+ * 排序后新产生的 RDD 的分区数与原 RDD 的分区数一致。中间存在 shuffle 的过程
+ */
+object Spark12_RDD_Operator_Transform {
+
+  def main(args: Array[String]): Unit = {
+
+    val sparkConf = new SparkConf().setMaster("local[*]").setAppName("Operator")
+    val sc = new SparkContext(sparkConf)
+
+    val rdd = sc.makeRDD(List(6, 2, 4, 5, 3, 1), 2)
+
+    val newRDD: RDD[Int] = rdd.sortBy(num => num)
+
+    newRDD.saveAsTextFile("output")
+
+
+    sc.stop()
+
+  }
+}
