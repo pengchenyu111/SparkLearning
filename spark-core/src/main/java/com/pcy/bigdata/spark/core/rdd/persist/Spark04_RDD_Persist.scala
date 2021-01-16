e4aacb457beb927b429460fbad1c40a6f07952e3
pengchenyu <1106998960@qq.com>
2021-01-16 11:43:06 +0800

    RDD检查点checkpoint.

---
+++ spark-core/src/main/java/com/pcy/bigdata/spark/core/rdd/persist/Spark04_RDD_Persist.scala
@@ -0,0 +1,45 @@
+package com.pcy.bigdata.spark.core.rdd.persist
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.storage.StorageLevel
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+ * 所谓的检查点其实就是通过将 RDD 中间结果写入磁盘
+ * 由于血缘依赖过长会造成容错成本过高，这样就不如在中间阶段做检查点容错，如果检查点之后有节点出现问题，可以从检查点开始重做血缘，减少了开销。
+ * 对 RDD 进行 checkpoint 操作并不会马上被执行，必须执行 Action 操作才能触发
+ */
+object Spark04_RDD_Persist {
+
+  def main(args: Array[String]): Unit = {
+    val sparConf = new SparkConf().setMaster("local").setAppName("Persist")
+    val sc = new SparkContext(sparConf)
+
+    // 设置检查点路径
+    sc.setCheckpointDir("cp")
+
+    val list = List("Hello Scala", "Hello Spark")
+
+    val rdd = sc.makeRDD(list)
+
+    val flatRDD = rdd.flatMap(_.split(" "))
+
+    val mapRDD = flatRDD.map(word => {
+      println("@@@@@@@@@@@@")
+      (word, 1)
+    })
+    // checkpoint 需要落盘，需要指定检查点保存路径
+    // 检查点路径保存的文件，当作业执行完毕后，不会被删除
+    // 一般保存路径都是在分布式存储系统：HDFS
+    mapRDD.checkpoint()
+
+    val reduceRDD: RDD[(String, Int)] = mapRDD.reduceByKey(_ + _)
+    reduceRDD.collect().foreach(println)
+    println("**************************************")
+    val groupRDD = mapRDD.groupByKey()
+    groupRDD.collect().foreach(println)
+
+
+    sc.stop()
+  }
+}
