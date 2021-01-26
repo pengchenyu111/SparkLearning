dfb40191b0f5e75fa8875b7156086389da7094d1
pengchenyu <1106998960@qq.com>
2021-01-26 21:36:28 +0800

    SparkSQL——连接MySQL.

--- spark-sql/src/main/java/com/pcy/bigdata/spark/sql/SparkSQL_JDBC.scala
+++ spark-sql/src/main/java/com/pcy/bigdata/spark/sql/SparkSQL_JDBC.scala
@@ -1,7 +1,8 @@
 package com.pcy.bigdata.spark.sql
 
+import java.util.Properties
+
 import org.apache.spark.SparkConf
-import org.apache.spark.sql.expressions.Aggregator
 import org.apache.spark.sql._
 
 object SparkSQL_JDBC {
@@ -13,27 +14,37 @@ object SparkSQL_JDBC {
     import spark.implicits._
 
     // 读取MySQL数据
+
+    // 方式1：通过load
     val df: DataFrame = spark.read
       .format("jdbc")
-      .option("url", "jdbc:mysql://49.232.218.99:3306/movie_recommendation")
-      .option("driver", "com.mysql.jdbc.Driver")
+      .option("url", "jdbc:mysql://49.232.218.99:3306/movie_recommendation?serverTimezone=UTC&useUnicode=true&characterEncoding=utf-8&useSSL=true")
+      .option("driver", "com.mysql.cj.jdbc.Driver")
       .option("user", "root")
-      .option("password", "123123")
-      .option("dbtable", "user")
+      .option("password", "xxxxxxxx")
+      .option("dbtable", "movie_user")
       .load()
-    //df.show
 
-    // 保存数据
-    df.write
+    // 方式2: 通用的 load 方法读取参数另一种形式
+    val df2: DataFrame = spark.read
       .format("jdbc")
-      .option("url", "jdbc:mysql://linux1:3306/spark-sql")
-      .option("driver", "com.mysql.jdbc.Driver")
-      .option("user", "root")
-      .option("password", "123123")
-      .option("dbtable", "user1")
-      .mode(SaveMode.Append)
-      .save()
+      .options(
+        Map(
+          "url" -> "jdbc:mysql://49.232.218.99:3306/movie_recommendation?serverTimezone=UTC&useUnicode=true&characterEncoding=utf-8&useSSL=true&user=root&password= xxxxxxxx",
+          "dbtable" -> "movie_user",
+          "driver" -> "com.mysql.cj.jdbc.Driver")
+      )
+      .load()
 
+    // 方式3: 使用 jdbc 方法读取
+    val props: Properties = new Properties()
+    props.setProperty("user", "root")
+    props.setProperty("password", "xxxxxxxx")
+    props.setProperty("driver", "com.mysql.cj.jdbc.Driver")
+    val df3: DataFrame = spark.read.jdbc("jdbc:mysql://49.232.218.99:3306/movie_recommendation?serverTimezone=UTC&useUnicode=true&characterEncoding=utf-8&useSSL=true", "movie_user", props)
+
+
+    df.show(10)
 
     spark.close()
   }
