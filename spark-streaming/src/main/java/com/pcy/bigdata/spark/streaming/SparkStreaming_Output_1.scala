a1eb883ca85659d2ebb2d5306c24476afb8be68a
pengchenyu <1106998960@qq.com>
2021-01-31 16:00:09 +0800

    SparkStreaming——输出.

---
+++ spark-streaming/src/main/java/com/pcy/bigdata/spark/streaming/SparkStreaming_Output_1.scala
@@ -0,0 +1,40 @@
+package com.pcy.bigdata.spark.streaming
+
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.dstream.DStream
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+object SparkStreaming_Output_1 {
+
+  def main(args: Array[String]): Unit = {
+
+    val sparkConf = new SparkConf().setMaster("local[*]").setAppName("SparkStreaming")
+    val ssc = new StreamingContext(sparkConf, Seconds(3))
+    ssc.checkpoint("cp")
+
+    val lines = ssc.socketTextStream("localhost", 9999)
+    val wordToOne = lines.map((_, 1))
+
+    val windowDS: DStream[(String, Int)] =
+      wordToOne.reduceByKeyAndWindow(
+        (x: Int, y: Int) => {
+          x + y
+        },
+        (x: Int, y: Int) => {
+          x - y
+        },
+        Seconds(9), Seconds(3))
+
+    // foreachRDD不会出现时间戳
+    // 在 foreachRDD()中，可以重用我们在 Spark 中实现的所有行动操作。比如，常见的用例之一是把数据写到诸如 MySQL 的外部数据库中。
+    windowDS.foreachRDD(
+      rdd => {
+
+      }
+    )
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
+
+}
