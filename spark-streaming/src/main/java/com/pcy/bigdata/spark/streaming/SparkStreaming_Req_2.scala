2462cbe0f10f0c14765eee82cba7b3efcf010b0d
pengchenyu <1106998960@qq.com>
2021-01-31 17:21:46 +0800

    SparkStreaming——案例需求.

---
+++ spark-streaming/src/main/java/com/pcy/bigdata/spark/streaming/SparkStreaming_Req_2.scala
@@ -0,0 +1,35 @@
+package com.pcy.bigdata.spark.streaming
+
+import org.apache.kafka.clients.consumer.{ConsumerConfig, ConsumerRecord}
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.dstream.InputDStream
+import org.apache.spark.streaming.kafka010.{ConsumerStrategies, KafkaUtils, LocationStrategies}
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+object SparkStreaming_Req_2 {
+
+  def main(args: Array[String]): Unit = {
+
+    val sparkConf = new SparkConf().setMaster("local[*]").setAppName("SparkStreaming")
+    val ssc = new StreamingContext(sparkConf, Seconds(3))
+
+    val kafkaPara: Map[String, Object] = Map[String, Object](
+      ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG -> "49.232.218.99:9092",
+      ConsumerConfig.GROUP_ID_CONFIG -> "test-consumer-group",
+      "key.deserializer" -> "org.apache.kafka.common.serialization.StringDeserializer",
+      "value.deserializer" -> "org.apache.kafka.common.serialization.StringDeserializer"
+    )
+
+    val kafkaDataDS: InputDStream[ConsumerRecord[String, String]] = KafkaUtils.createDirectStream[String, String](
+      ssc,
+      LocationStrategies.PreferConsistent,
+      ConsumerStrategies.Subscribe[String, String](Set("testTopic"), kafkaPara)
+    )
+    kafkaDataDS.map(_.value()).print()
+
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
+
+}
